42960e95ae5b7c37ced8c6156cfe7b09994ad354
Gabriel Simmer <gabriel@circleci.com>
2019-12-06 22:53:43 +0000

    Update script/function Implements a very simple self-updating function, which works against the GitHub API since we host all the files there anyways.

--- main.py
+++ main.py
@@ -3,7 +3,42 @@ import sys
 import os
 import glob
 from importlib import import_module
+import requests
+import hashlib
+import json
 
+
+# Self-updater
+# Abandon hope, all ye who enter (it's not that bad)
+def Update():
+    r = requests.get("https://api.github.com/repos/gmemstr/circleci-api-scripts/commits")
+    data = r.json()
+
+    with open("versions") as cache:
+        version_cache = json.load(cache)
+    # We need to update something, but what?
+    if "sha" not in version_cache:
+        version_cache['sha'] = ""
+        version_cache['modules'] = {}
+    if version_cache['sha'] != data[0]['sha']:
+        # Check modules for updates
+        r_mod = requests.get("https://api.github.com/repos/gmemstr/circleci-api-scripts/contents/modules")
+        data_mod = r_mod.json()
+        for module in data_mod:
+            name = module['name']
+            if name not in version_cache['modules']:
+                version_cache['modules'][name] = {"version": ""}
+            if version_cache['modules'][name]['version'] != module['sha']:
+                print("{name} updating...".format(name=name))
+                r_u = requests.get(module['download_url'])
+                f = open("modules/" + name, "w+")
+                f.write(r_u.text)
+                f.close()
+                version_cache['modules'][name]['version'] = module['sha']
+    # Finally, write new version file
+    version_cache['sha'] = data[0]['sha']
+    with open("versions", "w") as cache:
+        json.dump(version_cache, cache, indent=4)
 
 def GetModules(): 
     files = [f for f in glob.glob("modules/*.py")]
@@ -31,6 +66,9 @@ if __name__ == '__main__':
     if command == "modules":
         for module in modules:
             print(module)
+        exit()
+    if command == "update":
+        Update()
         exit()
 
     if command in modules:
