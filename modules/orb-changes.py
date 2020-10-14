a7e5d9261f8d653ae234ebf144628664e8316d45
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-10-14 15:54:16 +0100

    Completely rewrite orb diff'ing.
    Rewritten with deepdiff library, which does all of our heavy lifting.

--- modules/orb-changes.py
+++ modules/orb-changes.py
@@ -1,72 +1,80 @@
 import requests
 import json
 import yaml
+from deepdiff import DeepDiff
+import deepdiff
 
-# Pass in YAML dicts.
-def parse_version_diff(version_latest, version_previous, parent="latest"):
-    diffstring = ""
-    if version_latest is None or version_previous is None:
-        return ""
+# Pass in YAML strings.
+def parse_version_diff(version_latest, version_previous):
+    ddiff = DeepDiff(version_latest, version_previous)
+    final_string = ""
 
-    for key, value in version_latest.items():
-        if value is None:
-            continue
-        if isinstance(value, list):
-            continue
-        
-        if isinstance(version_previous.get(key), type(value)) == False:
-            if version_previous.get(key) is None:
-                diffstring += "+ added {key} to {parent}\n".format(key=key, parent=parent)
-                continue
-            diffstring += "* changed {key} to {type} in {parent}\n".format(key=key, type=type(version_latest.get(key)).__name__, parent=parent)
-            continue
-        if isinstance(value, dict):
-            diffstring += parse_version_diff(value, version_previous.get(key), parent +" > {}".format(key))
-            continue
-        if version_previous.get(key) is None:
-            diffstring += "+ added {key} to {parent}\n".format(key=key, parent=parent)
-        if value != version_previous.get(key):
-            diffstring += "* changed {parent} > {key} from {from} to {to}".format(parent,key,version_previous.get(key), value)
+    for key, value in ddiff.items():
+        prefix = "?"
+        if "removed" in key:
+            prefix = "Removed"
+        if "added" in key:
+            prefix = "Added"
+        if "changed" in key or "changes" in key:
+            prefix = "Changed"
+        if isinstance(value, deepdiff.model.PrettyOrderedSet):
+            for v in value:
+                v = v.replace("root", "").replace("][", " > ").replace("[", "").replace("]", " ").replace("'", "")
+                final_string += f"{prefix}: orb > {v}  \n"
+        elif isinstance(value, dict):
+            for k, v in value.items():
+                location = k.replace("root", "").replace("][", " > ").replace("[", "").replace("]", " ").replace("'", "")
+                if v.get("diff"):
+                    diff = v.get("diff")
+                    final_string += f"{prefix}: orb > {location}\n  ```diff\n{diff}\n```  \n"
+                else:
+                    new = v.get("new_value", "")
+                    old = v.get("old_value", "")
+                    final_string += f"{prefix}: orb > {location} \"{old}\" -> \"{new}\"  \n"
 
-    for key, value in version_previous.items():
-        if isinstance(version_latest.get(key), type(value)) == False:
-            if version_latest.get(key) is None:
-                diffstring += "- removed {key} from {parent}\n".format(key=key, parent=parent)
-                continue
-            diffstring += "* changed {key} to {type} in {parent}\n".format(key=key, type=type(version_latest.get(key)).__name__, parent=parent)
-            continue
-        if isinstance(value, dict):
-            diffstring += parse_version_diff(version_latest.get(key), value, parent +" > {}".format(key))
-            continue
-        if isinstance(value, list):
-            continue
-        if value is None:
-            continue
-        if version_latest.get(key) is None:
-            diffstring += "- removed {key} from {parent}\n".format(key=key, parent=parent)
-        if value != version_latest.get(key):
-            diffstring += "* changed {parent} > {key} from {from} to {to}".format(parent,key, value, version_latest.get(key))
-
-    return diffstring
+    return final_string
 
 def run_command(args):
+    if len(args) == 2:
+        return (
+            "A latest semver was provided, but no previous.\n"
+            "Usage: ./main.py orb-changes <namespace>/<orb-name> <latest-semver> <previous-semver>"
+        )
     query = """{
-      orb(name: "circleci/slack") {
+      orb(name: "%s") {
         id
-        versions(count: 2) {
+        versions(count: 25) {
           id
           version
           source
         }
       }
-    }"""
+    }""" % (args[0])
 
     url = 'https://circleci.com/graphql-unstable'
     r = requests.post(url, json={'query': query})
     json_data = json.loads(r.text)
+    latest_version = previous_version = None
 
-    latest_version = yaml.load(json_data["data"]["orb"]["versions"][0]["source"])
-    previous_version = yaml.load(json_data["data"]["orb"]["versions"][1]["source"])
+    if len(args) == 3:
+        new = args[1]
+        old = args[2]
+        print(f"Comparing {new} to {old}")
+        for version in json_data["data"]["orb"]["versions"]:
+            if version["version"] == args[1]:
+                latest_version = yaml.load(version["source"])
+            if version["version"] == args[2]:
+                previous_version = yaml.load(version["source"])
+    else:
+        latest_version = yaml.load(json_data["data"]["orb"]["versions"][1]["source"])
+        previous_version = yaml.load(json_data["data"]["orb"]["versions"][2]["source"])
+
+    if latest_version is None or previous_version is None:
+        return (
+            "Unable to find versions to compare, exiting.\n"
+            "Note that for performance, we only fetch the last 25 versions."
+        )
 
     diff = parse_version_diff(latest_version, previous_version)
-    return diff
+    version = json_data["data"]["orb"]["versions"][1]["version"]
+    return f"## v{version}\n\n{diff}"
